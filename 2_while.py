eba870ff25589ce22b695fa273dc5abbc979d74f
schmolh <hans-peter.schmollinger@online.de>
2025-01-16 17:04:12 +0100

    Update 2_while.py

--- 2_while.py
+++ 2_while.py
@@ -2,6 +2,11 @@ nterms = 10
 n1 = 0
 n2 = 1
 count = 0
+
+for i in range(5,20,2):
+    print(i)
+
+
 # while-Schleife: solange Kriterium erfüllt ist, wird Schleife durchlaufen
 while count < nterms: 
        # The next is a smart version of print(str(n1) + ' , ')
