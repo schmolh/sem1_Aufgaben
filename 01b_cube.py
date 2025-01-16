6e54273a9cc5a89e8ff7e75f3a2acb431888f97e
schmolh <hans-peter.schmollinger@online.de>
2025-01-16 17:08:47 +0100

    aa

--- 01b_cube.py
+++ 01b_cube.py
@@ -2,5 +2,6 @@ cubes = [1, 8, 27, 65, 125] # something's wrong here
 cubes[3] = 4 ** 3 # replace the wrong value
 cubes.append(7 ** 3) # and the cube of 7
 print(cubes )
+print(cubes[3] )
 
 # geben sie den Wert der Positon 2 mit einem print aus 
